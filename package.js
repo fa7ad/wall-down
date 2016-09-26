e7b55932d1e7cd78404c1c819d749da7412c3bd5
Fahad Hossain <oneloopone@gmail.com>
2016-09-26 17:42:26 +0600

    ignore node_modules

--- package.js
+++ package.js
@@ -1,9 +1,10 @@
 const NWBuilder = require('nw-builder')
 
 const nw = new NWBuilder({
-  files: ['app/**/*', '!app/src${/*}'],
+  files: ['app/**/*', '!app/src${/*}', '!**/node_modules/**'],
   version: 'latest',
-  platforms: ['win', 'linux'],
+  flavor: 'normal',
+  platforms: ['win32', 'linux'],
   macIcns: 'app/icon/icon.icns',
   winIco: 'app/icon/icon.ico'
 })
